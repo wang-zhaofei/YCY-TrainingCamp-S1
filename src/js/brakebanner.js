6ead22aab2689c11504605b6d3930ba0e6f6de9c
wzhaofei <wangzf@wellnkiot.com>
2022-07-29 22:48:10 +0800

    feat: 初始化画布

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -1,5 +1,12 @@
-class BrakeBanner{
-	constructor(selector){
-		
+class BrakeBanner {
+	constructor(selector) {
+		this.app = new PIXI.Application({
+			width: window.innerWidth,
+			height: window.innerHeight,
+			backgroundColor: 0x0000ff,
+			resizeTo: window
+		})
+
+		document.querySelector(selector).appendChild(this.app.view);
 	}
 }
