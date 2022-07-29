6324931d3adf802358be3c14bffc77136d0c7d75
wzhaofei <wangzf@wellnkiot.com>
2022-07-29 23:03:47 +0800

    feat: 创建资源加载器

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -9,5 +9,15 @@ class BrakeBanner {
 		})
 
 		document.querySelector(selector).appendChild(this.app.view);
+
+		// 创建资源加载器
+		this.loader = new PIXI.Loader();
+		this.loader.add('btn.png', 'images/btn.png');
+		this.loader.load();
+
+		this.loader.onComplete.add(() => {
+			const btnImage = new PIXI.Sprite(this.loader.resources['btn.png'].texture);
+			this.app.stage.addChild(btnImage);
+		});
 	}
 }
